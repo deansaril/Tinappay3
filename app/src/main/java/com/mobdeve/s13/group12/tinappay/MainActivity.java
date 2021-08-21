ff0b2c8880d3135f7066770dcd7af3c3bebe9915
deansaril <dean_miguel_saril@dlsu.edu.ph>
2021-08-22 01:34:32 +0800

    updated MainActivity

--- app/src/main/java/com/mobdeve/s13/group12/tinappay/MainActivity.java
+++ app/src/main/java/com/mobdeve/s13/group12/tinappay/MainActivity.java
@@ -2,13 +2,50 @@ package com.mobdeve.s13.group12.tinappay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvRegister;
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
     }
+
+    private void initComponents(){
+        this.tvRegister = findViewById(R.id.tv_login_register);
+        this.etEmail = findViewById(R.id.et_login_email);
+        this.etPassword = findViewById(R.id.et_login_password);
+        this.btnLogin = findViewById(R.id.btn_login_confirm);
+
+        this.tvRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        this.btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+            }
+        });
+    }
+
 }
