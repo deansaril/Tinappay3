e09c08f5120c00c2a4f2ce2c43eb72e3a54b7708
deansaril <dean_miguel_saril@dlsu.edu.ph>
2021-08-21 23:29:02 +0800

    Added logos

--- app/src/main/java/com/mobdeve/s13/group12/myapplication/MainActivity.java
+++ app/src/main/java/com/mobdeve/s13/group12/myapplication/MainActivity.java
@@ -6,9 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String HelloWorld = "Changed HEllo World";
-    private int nteger = 2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
