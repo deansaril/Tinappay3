8f138fb0a72745a011a0a4f6b870839cd4c975e5
deansaril <dean_miguel_saril@dlsu.edu.ph>
2021-08-24 20:53:52 +0800

    Updated Register and Account Settings related files

---
+++ app/src/main/java/com/mobdeve/s13/group12/tinappay/account/AccountActivity.java
@@ -0,0 +1,46 @@
+package com.mobdeve.s13.group12.tinappay.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.mobdeve.s13.group12.tinappay.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    private TextView tvEmail;
+    private EditText etPassword;
+    private Button btnChangePass;
+    private Button btnLogout;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+
+    private void initComponents(){
+        this.tvEmail = findViewById(R.id.tv_register_login);
+        this.etPassword = findViewById(R.id.et_register_email);
+        this.btnChangePass = findViewById(R.id.btn_register_confirm);
+        this.btnLogout = findViewById(R.id.btn_account_logout);
+
+        this.btnLogout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = etPassword.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/mobdeve/s13/group12/tinappay/account/RegisterActivity.java
+++ app/src/main/java/com/mobdeve/s13/group12/tinappay/account/RegisterActivity.java
@@ -2,15 +2,60 @@ package com.mobdeve.s13.group12.tinappay.account;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mobdeve.s13.group12.tinappay.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextView tvRegister;
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnConfirm;
+    private ProgressBar pbRegister;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    private void initComponents(){
+        this.tvRegister = findViewById(R.id.tv_register_login);
+        this.etEmail = findViewById(R.id.et_register_email);
+        this.etPassword = findViewById(R.id.et_register_password);
+        this.pbRegister = findViewById(R.id.pb_register);
+        this.btnConfirm = findViewById(R.id.btn_register_confirm);
+
+        this.tvRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        this.btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                /*
+                if(!checkEmpty(email, password){
+                    User user = new User(email, password);
+                    storeUser(user);
+                }
+                 */
+            }
+        });
     }
 }
